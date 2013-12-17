4ce307b82815567d742e6931131ae746f908a8e0
myspecialspace <ek.a.kuznetsova@gmail.com>
2013-12-17 19:11:47 +0400

    rails generate controller pages about and contact

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,5 +5,13 @@ class PagesControllerTest < ActionController::TestCase
     get :home
     assert_response :success
   end
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
 
 end
