d589b720f6cdba4ef3fa0ef8824321e82ef48c17
myspecialspace <ek.a.kuznetsova@gmail.com>
2014-07-14 17:43:55 +0400

    Authentication1

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,48 +3,40 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  
   def index
     @pins = Pin.all
   end
 
-  
   def show
   end
 
-  
   def new
     @pin = current_user.pins.build
   end
 
-  
   def edit
   end
 
-  
   def create
-      @pin = current_user.pins.build(pin_params)
-      if @pin.save
-        redirect_to @pin, notice: 'Pin was successfully created.' 
-      else
-        render action: 'new'
-      end
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
-  
   def update
-  
-      if @pin.update(pin_params)
-        redirect_to @pin, notice: 'Pin was successfully updated.' 
-      else
-        render action: 'edit' 
-      end
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
-  
   def destroy
     @pin.destroy
-    redirect_to pins_url 
+    redirect_to pins_url
   end
 
   private
@@ -60,6 +52,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
